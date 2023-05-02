5e2c66bd19d83a79b5d343397122a8f5a74dd4fa
viih_estheffany <107476769+viihestheffany@users.noreply.github.com>
2023-05-02 11:13:17 -0300

    Add files via upload

--- projeto loja/js/script.js
+++ projeto loja/js/script.js
@@ -5,11 +5,16 @@ const cs = (el) => document.querySelectorAll(el);
 modeloJson.map((item, index) => {
     let modeloItem = c('.models .modelo-item').cloneNode(true);
 
-
     modeloItem.querySelector('.modelo-item-img img').src = item.img;
     modeloItem.querySelector('.modelo-item-name').innerHTML = item.name;
     modeloItem.querySelector('.modelo-item-desc').innerHTML = item.description;
     modeloItem.querySelector('.modelo-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+
+    modeloItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDeFault();
+
+        c('.modeLoWindowArea').style.display = 'flex';
+    });
 
     c('.modelo-area').append(modeloItem);
 });

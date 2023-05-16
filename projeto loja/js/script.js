7ee0d56014dc33c21cd7c4e06405ffe2a98e7ec1
viih_estheffany <107476769+viihestheffany@users.noreply.github.com>
2023-05-16 11:02:05 -0300

    Add files via upload

--- projeto loja/js/script.js
+++ projeto loja/js/script.js
@@ -1,7 +1,9 @@
+let modalQt = 1;
+
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
-//Listagem dos modelos 
+//Listagem dos modelos
 modeloJson.map((item, index) => {
     let modeloItem = c('.models .modelo-item').cloneNode(true);
 
@@ -13,26 +15,70 @@ modeloJson.map((item, index) => {
     modeloItem.querySelector('.modelo-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
 
     modeloItem.querySelector('a').addEventListener('click', (e) => {
-        e.preventDeFault();
+        e.preventDefault();
 
-        let key =  e.target.closest('.modelo-item').getAttribute('data-key');
-
-        c('.modeloBig').src = modeloJson[key].img;
-        c('modeloInfo h1').innerHTML = modeloJson[key].name;
-        c('.modeloInfo-desc'),innerHTML = modeloJson[key].description;
-        c('.modeloInfo-actualPrice').innerHTML = `R$ ${modelojson[key].price.toFixed(2)}`;
+        let key = e.target.closest('.modelo-item').getAttribute('data-key');
+        modalQt = 1;
+        
+        c('.modeloBig img').src = modeloJson[key].img;
+        c('.modeloInfo h1').innerHTML = modeloJson[key].name;
+        c('.modeloInfo-desc').innerHTML = modeloJson[key].description;
+        c('.modeloInfo-actualPrice').innerHTML = `R$ ${modeloJson[key].price.toFixed(2)}`;
+        c('.modeloInfo-size.selected').classList.remove('selected');
 
         cs('.modeloInfo-size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 0){
+                size.classList.add('selected');
+            }
             size.querySelector('span').innerHTML = modeloJson[key].sizes[sizeIndex];
         });
 
+        c('.modeloInfo-qt').innerHTML = modalQt;
         c('.modeloWindowArea').style.opacity = 0;
         c('.modeloWindowArea').style.display = 'flex';
 
         setTimeout(() =>{
             c('.modeloWindowArea').style.opacity = 1;
-        },200);
+        }, 500);
     });
 
     c('.modelo-area').append(modeloItem);
+});
+
+//eventos Modal
+function closeModal(){
+    c('.modeloWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.modeloWindowArea').style.display = 'none';
+    }, 500);
+}
+
+cs('.modeloInfo-cancelButton, .modeloInfo-cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+});
+
+//botão qt menos
+c('.modeloInfo-qtmenos').addEventListener('click', () =>{
+    if(modalQt > 1){
+        modalQt--;
+        c('.modeloInfo-qt').innerHTML = modalQt;
+    }
+});
+
+//botão qt mais
+c('.modeloInfo-qtmais').addEventListener('click', () =>{
+    modalQt++;
+    //atualizo o valor no html
+    c('.modeloInfo-qt').innerHTML = modalQt;
+});
+
+//tamanhos
+cs('.modeloInfo-size').forEach((size, sizeIndex) =>{
+    //ao clicar em um item
+    size.addEventListener('click', () =>{
+        //desmarca tudo
+        c('.modeloInfo-size.selected').classList.remove('selected');
+        //adiciono a maarcação
+        size.classList.add('selected');
+    });
 });
